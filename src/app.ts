8327896318d61ceb3074e68999f62df00176a01b
Kehinde Bello <adedotunomomeji@gmail.com>
2025-01-30 13:56:01 +0100

    misc: log statement

--- src/app.ts
+++ src/app.ts
@@ -21,5 +21,5 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerOptions));
 app.use('/', router)
 
 app.listen(PORT, () => {
-    console.log(`App Listening on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 })
